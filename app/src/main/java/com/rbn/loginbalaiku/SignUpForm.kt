38155a187a7654a9e8044a11557edfe0c10b2c56
rbnpdg <aditamarubenpandega@gmail.com>
2024-06-20 11:36:34 +0700

    update bg sign up dan forgot psw

--- app/src/main/java/com/rbn/loginbalaiku/SignUpForm.kt
+++ app/src/main/java/com/rbn/loginbalaiku/SignUpForm.kt
@@ -1,5 +1,6 @@
 package com.rbn.loginbalaiku
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
@@ -7,6 +8,8 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -20,6 +23,14 @@ fun SignUpForm(navController: NavController, modifier: Modifier = Modifier) {
     val password = remember { mutableStateOf("") }
 
     Box(modifier = Modifier.fillMaxSize()) {
+        //Background
+        Image(
+            painter = painterResource(id = R.drawable.bglogin),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize(),
+            alignment = Alignment.Center
+        )
         Column(
             modifier = modifier
                 .fillMaxWidth()
